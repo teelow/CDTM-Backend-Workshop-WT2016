2a96828ed6a1fe078f620b52873c5684c037f18c
Michael Fröhlich <mike@Michaels-Air-3.fritz.box>
2016-12-07 11:41:29 +0100

    Add 009-server-front-end solution

--- solutions/server/server-008-serve-front-end/server.py
+++ solutions/server/server-008-serve-front-end/server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf8
 
-from flask import Flask
+from flask import Flask, send_file
 import sys
 
 # allow special characters (e.g. üäö ...)
@@ -15,8 +15,9 @@ sys.setdefaultencoding('utf-8')
 #   - We need this, so that the front-end works properly.
 app = Flask(__name__, static_url_path='')
 
-
-
+@app.route('/', methods=['GET'])
+def frontEnd():
+    return send_file('static/index.html')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=20008, debug=True)
